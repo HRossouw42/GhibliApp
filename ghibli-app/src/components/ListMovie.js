037586edd1ae4d07c3c4c4ef491bcbf61cf1cb9f
Harmun <harmun.rossouw@coil.com>
2022-06-25 16:17:10 +0100

    api calls working

--- ghibli-app/src/components/ListMovie.js
+++ ghibli-app/src/components/ListMovie.js
@@ -1,23 +1,16 @@
 // import totoro from "./bg-wave.svg";
 import totoro from '../images/Totoro.png';
+import poster from '../images/Poster.jpeg';
 
-function ListMovie() {
+function ListMovie({ movie }) {
     return (
         <div style={{}} className="listItem">
-            {/* <img src={totoro} alt="Nothing" /> */}
-            <h1>Title</h1>
-            <p className="description">
-                The orphan Sheeta inherited a mysterious crystal that links her
-                to the mythical sky-kingdom of Laputa. With the help of
-                resourceful Pazu and a rollicking band of sky pirates, she makes
-                her way to the ruins of the once-great civilization. Sheeta and
-                Pazu must outwit the evil Muska, who plans to use Laputa's
-                science to make himself ruler of the world.
-            </p>
+            <img src={movie.image} alt="None" className="posterImg" />
+
+            <h1>{movie.title}</h1>
+            <p className="description">{movie.description}</p>
             <button className="btn">SEE MORE</button>
-            <div>
-                <img src="" alt=""></img>
-            </div>
+            <img src={totoro} alt="Nothing" className="bgImg" />
         </div>
     );
 }
